f283e54eaf5ed5f7b2761c0df9ad0ebd646b237b
mazfa2806 <koplalj74@gmail.com>
2026-02-02 02:21:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwHxg_ir21pRQKVXi6q66yaSPx_Smii8UtRUPo4NS-zkUyiBByOwNuy0453herHp3bZxw/exec";
+const ADMIN_KEY = "mazfa2806";
+
+function loadProducts(isAdmin) {
+  fetch(API_URL)
+    .then(r => r.json())
+    .then(data => {
+      const list = document.getElementById("product-list");
+      list.innerHTML = "";
+      data.forEach((p, i) => {
+        list.innerHTML += `
+        <div class="card">
+          <h3>${p.Produk}</h3>
+          <p>Harga: ${p.Harga}</p>
+          <p>Stok: ${p.Stok}</p>
+          ${isAdmin
+            ? `<button onclick="deleteProduct(${i})">Hapus</button>`
+            : `<button onclick="buy('${p.Produk}',${p.Harga})">Beli</button>`
+          }
+        </div>`;
+      });
+    });
+}
+
+function buy(name, price) {
+  const msg = `Halo admin, saya mau beli ${name} harga ${price}`;
+  window.open(`https://wa.me/62XXXXXXXXXX?text=${encodeURIComponent(msg)}`);
+}
+
+function login() {
+  const pass = document.getElementById("adminPass").value;
+  if (pass === ADMIN_KEY) {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("admin-panel").style.display = "block";
+    loadProducts(true);
+  } else alert("Password salah");
+}
+
+function logout() {
+  location.reload();
+}
+
+function addProduct() {
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      key: ADMIN_KEY,
+      action: "add",
+      Produk: pname.value,
+      Harga: pprice.value,
+      Stok: pstock.value
+    })
+  }).then(() => loadProducts(true));
+}
+
+function deleteProduct(i) {
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      key: ADMIN_KEY,
+      action: "delete",
+      index: i
+    })
+  }).then(() => loadProducts(true));
+}
